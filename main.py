1c0457dab8156fa99a5aca909245ab3909f56a43
Michał Śmiech <michal.smiech1@gmail.com>
2022-12-03 14:23:10 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from mpi4py import MPI, rc
+import threading
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -7,10 +8,16 @@ print("rc.thread_level", rc.thread_level)
 
 print("rc.threads", rc.threads)
 
-if rank == 0:
+def send():
     data = {'a': 7, 'b': 3.14}
     comm.send(data, dest=1, tag=11)
     print(rank, 'sent')
-elif rank == 1:
+
+def receive():
     data = comm.recv(source=MPI.ANY_SOURCE, tag=11)
     print(rank, data)
+
+if rank == 0:
+    threading.Thread(target=send, args=()).start()
+elif rank == 1:
+    threading.Thread(target=receive, args=()).start()
