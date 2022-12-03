6b07b0de79c8eebeac2acd33b05ea070c880a697
Michał Śmiech <michal.smiech1@gmail.com>
2022-12-03 14:18:15 +0100

    commit

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ rank = comm.Get_rank()
 
 print("rc.thread_level", rc.thread_level)
 
+print("rc.threads", rc.threads)
+
 if rank == 0:
     data = {'a': 7, 'b': 3.14}
     comm.send(data, dest=1, tag=11)
     print(rank, 'sent')
 elif rank == 1:
-    data = comm.recv(source=0, tag=11)
+    data = comm.recv(source=MPI.ANY_SOURCE, tag=11)
     print(rank, data)
